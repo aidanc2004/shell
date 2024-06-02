83864f8125054440935ad13a56e507146c82f3be
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2024-06-01 22:44:07 -0300

    Fork child and run 'ls'

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
+/*
+ * Aidan Carey
+ * June 1st 2024
+ * main.c
+ */
+
 #include "stdio.h"
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/wait.h"
 
 int main() {
-  printf("Hello world!\n");
+  char *args[] = {"-a", NULL};
+  
+  pid_t pid = fork();
+  if (pid == 0) {
+    execvp("ls", args);
+  }
+
+  wait(NULL);
+
+  printf("done!\n");
+
   return 0;
 }
